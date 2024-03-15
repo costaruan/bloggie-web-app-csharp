c7f720496259a7814f2ae6cb134dcbc7f87f2943
costaruan <costaruan@outlook.com>
2024-03-14 22:07:25 -0300

    Implement WYSIWYG Editor and Image Upload

---
+++ Bloggie/Bloggie.Web/Controllers/ImagesController.cs
@@ -0,0 +1,31 @@
+﻿using Bloggie.Web.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace Bloggie.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImagesController : ControllerBase
+    {
+        private readonly IImageRepository imageRepository;
+
+        public ImagesController(IImageRepository imageRepository)
+        {
+            this.imageRepository = imageRepository;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UploadAsync(IFormFile file)
+        {
+            var imageURL = await imageRepository.UploadAsync(file);
+
+            if (imageURL == null)
+            {
+                return Problem("Sometihng went wrong!", null, (int)HttpStatusCode.InternalServerError);
+            }
+
+            return new JsonResult(new { link = imageURL });
+        }
+    }
+}
